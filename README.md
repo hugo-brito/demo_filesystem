c201d10921b54226a5966563ebffb1578bd41548
Hugo Brito <hubr@itu.dk>
2019-10-21 21:34:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# demo_filesystem
+ Mock file system built with Scala for training purposes.
---
+++ src/com/rtjvm/scala/oop/commands/Mkdir.scala
@@ -0,0 +1,29 @@
+package com.rtjvm.scala.oop.commands
+import com.rtjvm.scala.oop.files.Directory
+import com.rtjvm.scala.oop.filesystem.State
+
+class Mkdir(name: String) extends Command {
+
+	override def apply(state: State): State = {
+		val wd = state.wd
+		if (wd.hasEntry(name)) {
+			state.setMessage("Entry " + name + " already exists!")
+		} else if (name.contains(Directory.SEPARATOR)) {
+			state.setMessage(name + " must not contain separators!")
+		} else if (checkIllegal(name)) {
+			state.setMessage(name + ": illegal entry name!")
+		} else {
+			doMkdir(state, name)
+		}
+	}
+
+	def checkIllegal(name: String): Boolean = {
+		name.contains(".")
+	}
+
+	def doMkdir(state: State, name: String): State = {
+		val wd = state.wd
+		val fullPath = wd.path
+	}
+
+}
